9c75ddcf837419998e86fa1a552c56fca6104b64
David Kuc <David.Kuc@unit4.com>
2022-07-29 20:09:40 +0200

    Added google map in Contact page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "917f3a2ab8bbdf69"; }
+/******/ 		__webpack_require__.h = function() { return "a03de3f887cc7ef7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/kontakt/index.tsx
+++ src/pages/kontakt/index.tsx
@@ -1,10 +1,21 @@
+import TitleBar from "../../components/UI/TitleBar/TitleBar";
+import ContactInfo from "../../components/Contact/ContactInfo/ContactInfo";
+import Location from "../../components/Contact/Location/Location";
+
 function Kontakt() {
-    return (
-      <main>
-        <div>Kontakt</div>
-      </main>
-    );
-  }
-  
-  export default Kontakt;
-  
+  return (
+    <main>
+      <TitleBar>Kontakt</TitleBar>
+      <ContactInfo
+        phoneNr=" 791 058 767"
+        city="Wieliczka"
+        email="aniawolf@vp.pl"
+        postalCode="32-020"
+        street="Krakowska 4c"
+      />
+      <Location/>
+    </main>
+  );
+}
+
+export default Kontakt;
