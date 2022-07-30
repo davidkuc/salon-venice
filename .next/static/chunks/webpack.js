5c179d67c523d826d896e8542e56f7a97b5c3f35
David Kuc <David.Kuc@unit4.com>
2022-07-30 17:56:50 +0200

    Some style fixes for side nav + trying width in home page with viewport units

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fee19a0ae34e1290"; }
+/******/ 		__webpack_require__.h = function() { return "1978b6585afdd0ff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
